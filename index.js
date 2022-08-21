b5d706133291960e0936eaf053ef520eda4e0f26
filderschoice <filderschoice.dev@gmail.com>
2022-08-21 15:11:37 +0900

    feat: :sparkles: add cli args logics

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// Run Playwright
 const runPl = require('./runPlaywright')
-// Util
 const plUtil = require('./utils/plUtil')
+const cmd = require('commander')
+const pkg = require('./package.json')
 
+// cli args
+cmd
+  .version(pkg.version)
+  .option('-c, --config <configFile>', 'Use pl-leader Config File', './conf/plConfig.yaml')
+  .option('-s, --scenario <scenarioFile>', 'Use pl-leader Scenarios File', './conf/plScenarios.yaml')
+  .parse(process.argv)
+const option = cmd.opts()
+
+plUtil.logInfo('pl-reader start')
+plUtil.logInfo('  - config: ' + option.config)
+plUtil.logInfo('  - scenario: ' + option.scenario)
 // Scenario Read
-const plScenarios = plUtil.readFileSync('./conf/plScenarios.yaml', 'yaml')
+const plScenarios = plUtil.readFileSync(option.scenario, 'yaml')
 // Config Read
-const plOption = plUtil.readFileSync('./conf/plConfig.yaml', 'yaml')
+const plOption = plUtil.readFileSync(option.config, 'yaml')
 
 // main
 const main = async function () {
