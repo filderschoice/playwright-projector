3cbdf74b9b1770967063c42ff9e3333ae382a048
filderschoice <filderschoice.dev@gmail.com>
2022-09-02 09:14:17 +0900

    feat: :sparkles: split auth settings from config file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const pkg = require('./package.json')
 // cli args
 cmd
   .version(pkg.version)
+  .option('-a, --auth <authFile>', 'Use pl-projector Auth File', './conf/auth/plAuth.yaml')
   .option('-c, --config <configFile>', 'Use pl-projector Config File', './conf/plConfig.yaml')
   .option('-s, --scenario <scenarioFile>', 'Use pl-projector Scenarios File', './conf/plScenarios.yaml')
   .parse(process.argv)
@@ -13,11 +14,16 @@ const option = cmd.opts()
 
 plUtil.logInfo('pl-projector start')
 plUtil.logInfo('  - config: ' + option.config)
+plUtil.logInfo('  - auth: ' + option.auth)
 plUtil.logInfo('  - scenario: ' + option.scenario)
 // Scenario Read
 const plScenarios = plUtil.readFileSync(option.scenario, 'yaml')
 // Config Read
-const plOption = plUtil.readFileSync(option.config, 'yaml')
+const plConfig = plUtil.readFileSync(option.config, 'yaml')
+// Auth Read
+const plAuth = plUtil.readFileSync(option.auth, 'yaml')
+// Option Merge
+const plOption = { ...plConfig, ...plAuth }
 
 // main
 const main = async function () {
